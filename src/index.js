2b380b22b9170fd973a492e347d440d0c2a091c5
matheusdsanjos <matheusdsanjos@outlook.com>
2024-03-25 22:08:16 -0300

    Criação de toda a lógica para exibir nome e nível do herói

---
+++ src/index.js
@@ -0,0 +1,21 @@
+let heroi = ["Batman","6500"]
+let nivel
+
+if (heroi[1] < 1000)
+    nivel = "Ferro"
+else if (heroi[1] >= 1001 && heroi[1] <= 2000)
+    nivel = "Bronze"
+else if (heroi[1] >= 2001 && heroi[1] <= 5000)
+    nivel = "Prata"
+else if (heroi[1] >= 5001 && heroi[1] <= 7000)
+    nivel = "Ouro"
+else if (heroi[1] >= 7001 && heroi[1] <= 8000)
+    nivel = "Platina"
+else if (heroi[1] >= 8001 && heroi[1] <= 9000)
+    nivel = "Ascendente"
+else if (heroi[1] >= 9001 && heroi[1] <= 10000)
+    nivel = "Imortal"
+else if (heroi[1] >= 100000)
+    nivel = "Radiante"
+
+console.log(`O Herói ${heroi[0]} está no nível ${nivel}!`);
